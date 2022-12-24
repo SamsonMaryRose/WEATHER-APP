3a5fb01f8a8d86a7624b8b125aa2c3d7a52b9a18
SamsonMaryRose <ms2777037@gmail.com>
2022-12-23 21:00:24 -0800

    Updated api key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "4ec5c0904e05f548051f556cad0e94b8&fbclid"
+        SECRET_KEY: "aafadd2a2e4b78ece66f7feaa642cb47"
     }
 }
 
